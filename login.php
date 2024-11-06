f51a3d75b250584d20239c8fa5a5b02eca70e9ca
gonzxph <gonzxph@gmail.com>
2024-11-06 17:35:01 +0800

    create login

--- login.php
+++ login.php
@@ -8,12 +8,21 @@
     <?php include 'header/nav.php'?> 
 
     <div class="container">
-        <div class="login">
-            <form action="" method="post">
-            <div data-mdb-input-init class="form-outline mb-4">
-            <input type="email" id="form1Example13" class="form-control form-control-lg" />
-            <label class="form-label" for="form1Example13">Email address</label>
-          </div>
+        <div class="card mx-auto mt-5" style="max-width: 350px;">
+            <h2 class="mx-auto mt-3">Login</h2>
+            <form action="" method="post" class="m-3">
+            
+                <div class="form-group mb-3">
+                    <label class="form-label" for="email">Email address</label>
+                    <input type="email" id="email" name="email" class="form-control" placeholder="john.doe@email.com"/>
+                </div>
+                <div class="form-group mb-3">
+                    <label class="form-label" for="password">Password</label>
+                    <input type="password" id="password" name="password" class="form-control" placeholder="*******" />
+                </div>
+                <a href="#" class="float-end mb-3">Forgot password?</a><br>
+                <button type="submit" class="btn btn-primary mt-3 mx-auto d-block">Login</button>
+
             </form>
         </div>
     </div>
