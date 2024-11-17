6af8c5e17167e0ade637aa34494738ad2991b6a6
gonzxph <gonzxph@gmail.com>
2024-11-17 09:39:21 +0800

    added error when fields are empty

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
                     <div class="card text-center shadow-lg p-4" style="width: 25rem;">
                         <h5 class="card-title mb-3">Find the right car now!</h5>
                         <div class="card-body">
-                            <form action="index.php" method="POST">
+                            <form id="bookingForm" action="index.php" method="POST">
                                 <div class="input-group mb-3">
                                     <span class="input-group-text bg-white"><i class="fas fa-map-marker-alt text-warning"></i></span>
                                     <input readonly id="pickupInput" name="pickupinput" type="text" data-bs-toggle="modal" data-bs-target="#pickupModal" class="form-control" placeholder="Choose pick up location" value="">
@@ -34,7 +34,10 @@
                                 </div>
                                 <div class="input-group mb-3">
                                     <span class="input-group-text bg-white"><i class="fas fa-calendar-alt text-secondary"></i></span>
-                                    <input readonly type="text" data-bs-toggle="modal" data-bs-target="#dateTimeModal" class="form-control" placeholder="Choose date and time">
+                                    <input readonly id="dateTimeInput" type="text" data-bs-toggle="modal" data-bs-target="#dateTimeModal" class="form-control" placeholder="Choose date and time">
+                                </div>
+                                <div id="warningMessage" class="text-danger m-3" style="display: none;">
+                                    Please fill out all fields before submitting!
                                 </div>
                                 <button type="submit" class="btn btn-dark mt-3">Book Now</button>
                             </form>
@@ -112,27 +115,28 @@
 
 <!-- Pickup date and time modal -->
 <div class="modal fade" id="dateTimeModal" tabindex="-1" aria-labelledby="dateTimeModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered"> <!-- Adjusted modal size and centering -->
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="dateTimeModalLabel">Select Pickup & Drop-off Dates and Times</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <!-- Vanilla Calendar Container for Pick-up Date -->
-                <div id="vanillaCalendar" class="vanilla-calendar"></div>
+                <!-- Centered Calendar Container for Pick-up Date -->
+                <div id="vanillaCalendar" class="vanilla-calendar calendar-center"></div>
+                
                 <!-- Time Picker -->
-                <div class="row">
+                <div class="row mt-4">
                     <div class="col">
                         <label for="pickupTimeInput">Pickup Time:</label>
-                        <input type="text" class="form-control" id="pickupTimeInput" readonly>
+                        <input type="text" class="form-control" id="pickupTimeInput" readonly value="">
                     </div>
                     <div class="col">
                         <label for="dropOffTimeInput">Drop-Off Time:</label>
-                        <input type="text" class="form-control" id="dropOffTimeInput" readonly>
+                        <input type="text" class="form-control" id="dropOffTimeInput" readonly value="">
                     </div>
                 </div>
-                <p class="text-muted mt-2">Please choose a pickup time that is at least 1 hour from the current time.</p>
+                <p class="text-muted mt-3">Please choose a pickup time that is at least 1 hour from the current time.</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-primary" id="pconfirm">Confirm</button>
@@ -140,6 +144,7 @@
         </div>
     </div>
 </div>
+
 
 
     <?php include 'footer/footer.php' ?>
